934b477ceebce8ee788fba4e2546d4fd03f9a893
akshitha-2005 <akshitha.r2005@gmail.com>
2026-01-17 21:04:28 +0530

    created a side navigation for categories

---
+++ src/app/components/home/sidenavigation/sidenavigation.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidenavigation',
+  imports: [],
+  templateUrl: './sidenavigation.component.html',
+  styleUrl: './sidenavigation.component.css',
+})
+export class SidenavigationComponent {}
