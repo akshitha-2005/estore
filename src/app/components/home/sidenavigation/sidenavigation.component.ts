889947c1ba1c1c4f457313672cbff0395a314025
akshitha-2005 <akshitha.r2005@gmail.com>
2026-01-18 13:55:49 +0530

    created a sample data and fetched to display on side navigation

--- src/app/components/home/sidenavigation/sidenavigation.component.ts
+++ src/app/components/home/sidenavigation/sidenavigation.component.ts
@@ -1,3 +1,5 @@
+import { CategoryService } from './../services/category.service';
+import { Category } from '../types/category';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,4 +8,16 @@ import { Component } from '@angular/core';
   templateUrl: './sidenavigation.component.html',
   styleUrl: './sidenavigation.component.css',
 })
-export class SidenavigationComponent {}
+export class SidenavigationComponent {
+  categories: Category[] = [];
+
+  constructor(categoryService: CategoryService) {
+    this.categories = categoryService.getAllCategories();
+  }
+
+  getCategories(parentCategoryId?: number): Category[] {
+    return this.categories.filter(
+      (category) => category.parent_category_id === parentCategoryId
+    );
+  }
+}
