d78dd0f41e58dab553041a27d89f05a1cc8a99d8
akshitha-2005 <akshitha.r2005@gmail.com>
2026-01-20 12:06:00 +0530

    Installed the mysql2 driver, created a connection pool, successfully connected Node.js API to MySQL

--- estore-server/index.js
+++ estore-server/index.js
@@ -1,14 +1,26 @@
 const express = require("express");
+const mysql = require("mysql2");
 const app = express();
 const PORT = 5001;
 
+//Creating a pool is a best practise for handling multiple database queries efficiently.
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "*******",
+  database: "estore",
+  port: 3306,
+  multipleStatements: true,
+});
+
 app.get("/", (req, res) => {
-  let prodData = {
-    pName: "Jacket",
-    price: 50,
-    img: "shop-1,jpg",
-  };
-  res.status(200).send(prodData);
+  pool.getConnection((err, connention) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send("Connection Established");
+    }
+  });
 });
 
 const server = app.listen(PORT, () => {
