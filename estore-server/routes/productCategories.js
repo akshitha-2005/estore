4180c59a7fce8e4fe78e8b5e55635c86506b3ac5
akshitha-2005 <akshitha.r2005@gmail.com>
2026-01-22 23:11:31 +0530

    created routes to fetch products

--- estore-server/routes/productCategories.js
+++ estore-server/routes/productCategories.js
@@ -1,16 +1,6 @@
 const express = require("express");
-const mysql = require("mysql2");
+const pool = require("../shared/pool");
 const productCategories = express.Router();
-
-//Creating a pool is a best practise for handling multiple database queries efficiently.
-const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "Blackbottle$123",
-  database: "estore",
-  port: 3306,
-  multipleStatements: true,
-});
 
 productCategories.get("/", (req, res) => {
   pool.query("select * from categories", (error, categories) => {
---
+++ estore-server/routes/products.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const products = express.Router();
+const pool = require("../shared/pool");
+const { error } = require("console");
+
+products.get("/", (req, res) => {
+  pool.query("select * from products", (error, products) => {
+    if (error) res.status(500).send(error);
+    else res.status(200).send(products);
+  });
+});
+
+products.get("/:id", (req, res) => {
+  let id = req.params.id;
+  pool.query("select * from products where id = ?", [id], (error, products) => {
+    if (error) res.status(500).send(error);
+    else res.status(200).send(products);
+  });
+});
+
+module.exports = products;
