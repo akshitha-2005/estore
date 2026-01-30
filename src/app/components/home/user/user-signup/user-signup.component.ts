a1484aa440979f07d497c887b7630d9d369051b2
akshitha-2005 <akshitha.r2005@gmail.com>
2026-01-30 22:49:36 +0530

    added validators for user signup form

--- src/app/components/home/user/user-signup/user-signup.component.ts
+++ src/app/components/home/user/user-signup/user-signup.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
+import {
+  ReactiveFormsModule,
+  FormBuilder,
+  FormGroup,
+  AbstractControl,
+  Validators,
+} from '@angular/forms';
+import { matchPasswords } from './validators/match-passwords.validator';
 
 @Component({
   selector: 'app-user-signup',
@@ -11,16 +18,34 @@ export class UserSignupComponent {
   userSignupForm: FormGroup;
 
   constructor(private fb: FormBuilder) {
-    this.userSignupForm = this.fb.group({
-      firstName: [''],
-      lastName: [''],
-      address: [''],
-      city: [''],
-      state: [''],
-      pin: [''],
-      email: [''],
-      password: [''],
-      confirmPassword: [''],
-    });
+    this.userSignupForm = this.fb.group(
+      {
+        firstName: ['', Validators.required],
+        lastName: [''],
+        address: [''],
+        city: [''],
+        state: [''],
+        pin: [''],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', Validators.required],
+        confirmPassword: ['', Validators.required],
+      },
+      {
+        validator: matchPasswords,
+      },
+    );
+  }
+
+  get firstName(): AbstractControl<any, any> | null {
+    return this.userSignupForm.get('firstName');
+  }
+  get email(): AbstractControl<any, any> | null {
+    return this.userSignupForm.get('email');
+  }
+  get password(): AbstractControl<any, any> | null {
+    return this.userSignupForm.get('password');
+  }
+  get confirmPassword(): AbstractControl<any, any> | null {
+    return this.userSignupForm.get('confirmPassword');
   }
 }
